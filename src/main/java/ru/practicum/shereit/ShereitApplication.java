ad34139878adf53199a161fb8bbed78a61449f40
EVMGit <macbook@air>
2023-07-07 04:36:11 +0500

    init project

---
+++ src/main/java/ru/practicum/shereit/ShereitApplication.java
@@ -0,0 +1,13 @@
+package ru.practicum.shereit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShereitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShereitApplication.class, args);
+	}
+
+}
